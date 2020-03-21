5e3756e05a16b20bee12aafcd65a99e18a96981f
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-03-21 22:44:02 +0530

    Add files via upload

---
+++ python/basic/largenst elementOflist.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 22:09:13 2020
+
+@author: don_quixote
+"""
+
+
+""" 3. Write a Python program to get the largest number from a list. """
+
+
+l = [1,4,3,5,6,4,6,97,44,33,55,33,2,7,4,4,3,8,5]
+n = sorted(l)
+print(n[-1])
