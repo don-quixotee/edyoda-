7af3ffa6f250e10896f1381daca576babbb356db
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-03-21 22:42:25 +0530

    Add files via upload

---
+++ python/basic/everythisrd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 22:04:02 2020
+
+@author: don_quixote
+"""
+
+
+""" 3. Write a Python program to remove and print every third number from a list of numbers until the list becomes empty. """
+
+l = [1,3,5,4,6,4,4,6,4,43,2,3,4,5,6,7,96,5,4,4,5,43,53]
+print(l[::3])
