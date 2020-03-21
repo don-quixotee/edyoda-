0aa64b2d2bb328c60dbfc3afbc89b903bc4ad90f
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-03-21 22:41:54 +0530

    Add files via upload

---
+++ python/basic/printString.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 21:43:30 2020
+
+@author: don_quixote
+"""
+
+
+"""  1. Write a Python program to print the following string in a specific format (see the output). Go to the editor
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are  """
+    
+    
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+  
