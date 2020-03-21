9593c782b5e4da30d106dcb26f6a2795d0bfb23f
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-03-21 22:44:30 +0530

    Add files via upload

---
+++ python/removeduplicate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 22:36:28 2020
+
+@author: don_quixote
+"""
+
+
+""" 7. Write a Python program to remove duplicates from a list."""
+
+
+l= [ 1,3,5,5,3,4,6,6,4,3,5,7,7,4,43,3,4,5]
+
+print(list(set(l)))
