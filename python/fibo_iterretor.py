3ad6e579f3482051c12802765365104473e93593
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-05-24 08:30:03 +0530

    Add files via upload

---
+++ python/fibo_iterretor.py
@@ -0,0 +1,23 @@
+
+def fibo():
+    output = []
+    num1 = 0
+    num2 = 1
+    output.append(num1)
+    output.append(num2)
+    count = 2
+    while(True):
+        next_num = num1 + num2
+        output.append(next_num)
+        count+=1
+        
+        if count == 5:        
+            yield output
+            output = []
+            count = 0
+        num1,num2 = num2,next_num 
+        
+g = fibo()
+print(next(g))
+print(next(g))
+print(next(g))
